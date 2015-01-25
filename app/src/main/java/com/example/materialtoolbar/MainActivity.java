83c2eb85c247b861000fb650e5459ecfd30d8a72
LaymanDroid <laymandroid@gmail.com>
2015-01-25 14:43:13 -0500

    Add Toolbar and basic colors

--- app/src/main/java/com/example/materialtoolbar/MainActivity.java
+++ app/src/main/java/com/example/materialtoolbar/MainActivity.java
@@ -2,16 +2,22 @@ package com.example.materialtoolbar;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
 
